8826f144e5ae33a2000f63dd76da19c6a964ef4d
chadstewart <chadrhonanstewart@gmail.com>
2015-04-15 10:41:43 -0400

    Updated Course to default class structure

--- src/edu/nova/csis3460/eadvisor/courses/Course.java
+++ src/edu/nova/csis3460/eadvisor/courses/Course.java
@@ -1,5 +1,25 @@
 package edu.nova.csis3460.eadvisor.courses;
 
-public class Course {
-	Course (){}
+import java.util.*;
+
+/**
+/*  Course  
+*/  
+
+public class Course{
+
+    public Course(){
+    } 
+     
+    public void print(){
+        System.out.println("Hello World");
+    }
+    public static void test(){
+    	Course course  = new Course(); 
+        aClassName.print();
+    }
+
+    public static void main(String[] args){
+    	Course.test();
+    }  
 }
